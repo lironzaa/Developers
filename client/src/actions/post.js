2fb9f807a5f21405f5c2a96d9a77fd510d9c0c5d
lironzaa <lironzaa@gmail>
2020-02-18 13:36:39 +0200

    add post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types';
 
 export const getPosts = () => async dispatch => {
   try {
@@ -52,7 +52,7 @@ export const removeLike = id => async dispatch => {
 
 export const deletePost = id => async dispatch => {
   try {
-    const res = await axios.delete(`/api/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -61,6 +61,31 @@ export const deletePost = id => async dispatch => {
 
     dispatch(setAlert('Post Removed', 'success'));
   } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    })
+  }
+}
+
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  try {
+    const res = await axios.post(`/api/posts`, formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    })
+
+    dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    console.log(err);
     dispatch({
       type: POST_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -14,14 +14,14 @@ const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date } }) =>
   <div className="post bg-white p-1 my-1">
     <div>
-      <a href="profile.html">
+      <Link to={`/profile/${user}`}>
         <img
           className="round-img"
           src={avatar}
           alt=""
         />
         <h4>{name}</h4>
-      </a>
+      </Link>
     </div>
     <div>
       <p className="my-1">
@@ -54,7 +54,7 @@ const PostItem = ({
         </button>
       )}
     </div>
-  </div>
+  </div >
 
 PostItem.propTypes = {
   auth: PropTypes.object.isRequired,
