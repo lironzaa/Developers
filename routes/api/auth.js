57899c91602458925b1f66eb781e0e1ad73c328d
lironzaa <lironzaa@gmail.com>
2022-11-06 17:08:37 +0200

    m

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -22,6 +22,7 @@ router.post('/', [
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Password is required').exists()
 ], async (req, res) => {
+    console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
